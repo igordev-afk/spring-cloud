182fbe00cf71661a57d7a9eeef69933d74b60734
wwerlosh <igor.shverlo@yandex.ru>
2023-11-17 23:22:22 +0300

    init registry

---
+++ notification-service/src/main/java/ru/wwerlosh/notification/NotificationApplication.java
@@ -0,0 +1,7 @@
+package ru.wwerlosh.notification;
+
+public class NotificationApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ registry/src/main/java/ru/wwerlosh/registry/RegistryApplication.java
@@ -0,0 +1,7 @@
+package ru.wwerlosh.registry;
+
+public class RegistryApplication {
+    public static void main(String[] args) {
+
+    }
+}
